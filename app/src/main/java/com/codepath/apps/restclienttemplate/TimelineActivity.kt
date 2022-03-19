4707b16b599ff3005b7ff437dc1bf859c8dcdff0
YerBoySisi <YerBoySisi@users.noreply.github.com>
2022-03-19 02:19:57 -0400

    updated timestamp functionality

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
@@ -19,6 +20,8 @@ class TimelineActivity : AppCompatActivity() {
     lateinit var rvTweets: RecyclerView
     lateinit var adapter: TweetAdapter
 
+    lateinit var swipeContainer: SwipeRefreshLayout
+
     val tweets = ArrayList<Tweet>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +32,19 @@ class TimelineActivity : AppCompatActivity() {
 
         rvTweets = findViewById(R.id.rv_tweets)
         adapter = TweetAdapter(tweets)
+
+        swipeContainer = findViewById(R.id.swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            Log.i(TAG, "Refreshing timeline")
+            populateHomeTimeline()
+        }
+
+        swipeContainer.setColorSchemeResources(
+            android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light,
+        )
 
         rvTweets.layoutManager = LinearLayoutManager(this)
         rvTweets.adapter = adapter
@@ -45,9 +61,14 @@ class TimelineActivity : AppCompatActivity() {
                 val jsonArray = json.jsonArray
 
                 try {
+                    //clears currently fetched tweets
+                    adapter.clear()
+
                     val listOfNewTweetsRetrieved = Tweet.fromJsonArray(jsonArray)
                     tweets.addAll(listOfNewTweetsRetrieved)
                     adapter.notifyDataSetChanged()
+
+                    swipeContainer.isRefreshing = false
                 } catch(e: JSONException) {
                     Log.e(TAG, "JSON Exception $e")
                 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.kt
@@ -1,15 +1,21 @@
 package com.codepath.apps.restclienttemplate
 
+import android.text.format.DateUtils
 import android.view.LayoutInflater
-import android.view.ViewGroup
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.codepath.apps.restclienttemplate.models.Tweet
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class TweetAdapter(val tweets: List<Tweet>) : RecyclerView.Adapter<TweetAdapter.ViewHolder>() {
+
+class TweetAdapter(private val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<TweetAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetAdapter.ViewHolder {
         val context = parent.context
@@ -28,7 +34,7 @@ class TweetAdapter(val tweets: List<Tweet>) : RecyclerView.Adapter<TweetAdapter.
         // set item views based on views and data model
         holder.tvUsername.text = tweet.user?.name
         holder.tvTweetBody.text = tweet.body
-
+        holder.tvRelativeTime.text = getRelativeTimeAgo(tweet.createdAt)
         Glide.with(holder.itemView).load(tweet.user?.publicImageUrl).into(holder.ivProfileImage)
 
     }
@@ -37,10 +43,38 @@ class TweetAdapter(val tweets: List<Tweet>) : RecyclerView.Adapter<TweetAdapter.
         return tweets.size
     }
 
+    fun clear() {
+        tweets.clear()
+        notifyDataSetChanged()
+    }
+
+    fun addAll(tweetList: List<Tweet>) {
+        tweets.addAll(tweetList)
+        notifyDataSetChanged()
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val ivProfileImage = itemView.findViewById<ImageView>(R.id.profile_pic)
-        val tvUsername = itemView.findViewById<TextView>(R.id.username)
-        val tvTweetBody = itemView.findViewById<TextView>(R.id.tweet_body)
+        val ivProfileImage: ImageView = itemView.findViewById(R.id.profile_pic)
+        val tvUsername: TextView = itemView.findViewById(R.id.username)
+        val tvTweetBody: TextView = itemView.findViewById(R.id.tweet_body)
+        val tvRelativeTime: TextView = itemView.findViewById(R.id.timestamp)
+    }
+
+    fun getRelativeTimeAgo(rawJsonDate: String?): String? {
+        val twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy"
+        val sf = SimpleDateFormat(twitterFormat, Locale.ENGLISH)
+        sf.isLenient = true
+        var relativeDate = ""
+        try {
+            val dateMillis: Long = sf.parse(rawJsonDate).time
+            relativeDate = DateUtils.getRelativeTimeSpanString(
+                dateMillis,
+                System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS
+            ).toString()
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return relativeDate
     }
 
 }
